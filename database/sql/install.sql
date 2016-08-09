09a99f93c496e1b025a48eecc504443340e5ac7b
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-08-09 14:59:45 +0200

    create 'competition' table migration

--- database/sql/install.sql
+++ database/sql/install.sql
@@ -11,35 +11,6 @@ USE `weinstein` ;
 
 
 -- -----------------------------------------------------
--- Table `weinstein`.`competition`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `weinstein`.`competition` ;
-
-CREATE TABLE IF NOT EXISTS `weinstein`.`competition` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `label` VARCHAR(50) NULL,
-  `wuser_username` VARCHAR(80) NULL,
-  `created_at` TIMESTAMP NULL,
-  `updated_at` TIMESTAMP NULL,
-  `competitionstate_id` INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`),
-  INDEX `fk_competition_wuser1_idx` (`wuser_username` ASC),
-  UNIQUE INDEX `label_UNIQUE` (`label` ASC),
-  INDEX `fk_competition_competitionstate1_idx` (`competitionstate_id` ASC),
-  CONSTRAINT `fk_competition_wuser1`
-    FOREIGN KEY (`wuser_username`)
-    REFERENCES `weinstein`.`wuser` (`username`)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_competition_competitionstate1`
-    FOREIGN KEY (`competitionstate_id`)
-    REFERENCES `weinstein`.`competitionstate` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `weinstein`.`rating`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `weinstein`.`rating` ;
