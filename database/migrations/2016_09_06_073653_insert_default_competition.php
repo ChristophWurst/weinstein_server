784753723a9439127cfbf80c3f035065bbed9583
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-09-06 10:12:42 +0200

    add a default competition

---
+++ database/migrations/2016_09_06_073653_insert_default_competition.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\MasterData\CompetitionState;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class InsertDefaultCompetition extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		if (DB::table('competition')->where('label', 'Weinwoche')->count() > 0) {
+			return;
+		}
+
+		DB::table('competition')->insert([
+			[
+				'label' => 'Weinwoche',
+				'competition_state_id' => CompetitionState::STATE_ENROLLMENT,
+				'wuser_username' => null,
+			],
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		
+	}
+
+}
