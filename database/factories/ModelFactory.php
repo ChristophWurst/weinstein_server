f17b1f76e14dd541fe43d4d5f12963acab0459e1
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-09-06 20:07:25 +0200

    add integration tests for listing and resetting competitions

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -3,6 +3,8 @@
 use App\MasterData\Address;
 use App\MasterData\Applicant;
 use App\MasterData\Association;
+use App\MasterData\Competition;
+use App\MasterData\CompetitionState;
 use App\MasterData\User;
 use Faker\Generator as FakeData;
 
@@ -61,5 +63,14 @@ $factory->define(Address::class,
 		'nr' => $faker->numberBetween(1, 300),
 		'zipcode' => $faker->numberBetween(1000, 9999),
 		'city' => $faker->city,
+	];
+});
+
+$factory->define(Competition::class,
+	function() {
+	return [
+		'label' => str_random(10),
+		'competition_state_id' => CompetitionState::STATE_ENROLLMENT,
+		'wuser_username' => null,
 	];
 });
---
+++ tests/Integration/Settings/CompetitionTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License,version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace Test\Integration\Settings;
+
+use App\MasterData\Competition;
+use App\MasterData\CompetitionState;
+use App\MasterData\User;
+use Test\TestCase;
+use function factory;
+
+class CompetitionTest extends TestCase {
+
+	use \Illuminate\Foundation\Testing\DatabaseTransactions;
+
+	public function testShowCompetitions() {
+		$admin = factory(User::class, 'admin')->make();
+
+		$this->be($admin);
+
+		$this->get('settings/competitions');
+		$this->assertResponseOk();
+		$this->see('Bewerb');
+	}
+
+	public function testResetCompetition() {
+		$admin = factory(User::class, 'admin')->make();
+
+		$competition = factory(Competition::class)->create([
+			'competition_state_id' => CompetitionState::STATE_EXCLUDE,
+		]);
+
+		$this->be($admin);
+		$this->get('competition/' . $competition->id . '/reset');
+		$this->assertResponseOk();
+		$this->post('competition/' . $competition->id . '/reset', [
+			'reset' => 'Nein',
+		]);
+		$this->assertRedirectedTo('settings/competitions');
+
+		$this->get('competition/' . $competition->id . '/reset');
+		$this->assertResponseOk();
+		$this->post('competition/' . $competition->id . '/reset', [
+			'reset' => 'Ja',
+		]);
+		$this->assertRedirectedTo('settings/competitions');
+
+		$this->seeInDatabase('competition',
+			[
+			'id' => $competition->id,
+			'competition_state_id' => CompetitionState::STATE_ENROLLMENT,
+		]);
+	}
+
+}
