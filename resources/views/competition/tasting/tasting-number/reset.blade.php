56706cd4d32b1201d8fdd77b7280f0e69ec5417b
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-02-24 17:30:40 +0100

    fix typo
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- resources/views/competition/tasting/tasting-number/reset.blade.php
+++ resources/views/competition/tasting/tasting-number/reset.blade.php
@@ -4,7 +4,7 @@
 <h1>Kostnummern zur&uuml;cksetzen</h1>
 {!! Form::open() !!}
 <div class="form-group">
-		Sind Sie sicher, dass sie <strong>alle Kostnummern</strong> zur&uu&uuml;cksetzen m&ouml;chten?<br>
+		Sind Sie sicher, dass sie <strong>alle Kostnummern</strong> zur&uuml;cksetzen m&ouml;chten?<br>
         {!! Form::submit('Ja', array('name' => 'reset', 'class' => 'btn btn-default')) !!}
         {!! Form::submit('Nein', array('name' => 'reset', 'class' => 'btn btn-default')) !!}
 </div>
