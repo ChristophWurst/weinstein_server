429e61dd53d0146de600f75f1d7a61831ceca279
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-04 16:44:17 +0100

    Add new competition state to DB
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ database/migrations/2017_03_04_152647_create_catalogue_assignment_competition_state.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateCatalogueAssignmentCompetitionState extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		DB::table('competition_state')
+			->where('id', 10)
+			->update(['id' => 11]);
+		DB::table('competition_state')->insert([
+			'id' => 10,
+			'description' => 'CATALOGUE_NUMBERS',
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		DB::table('competition_state')
+			->where('id', 10)
+			->delete();
+		DB::table('competition_state')
+			->where('id', 11)
+			->update(['id' => 10]);
+	}
+
+}
