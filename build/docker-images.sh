4c0de51944e9d4e5e75858233c1cb56086035879
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-22 22:45:14 +0100

    Build docker images on all branches and tag them accordingly
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- build/docker-images.sh
+++ build/docker-images.sh
@@ -2,12 +2,18 @@
 
 set -ev
 
-if [ "$TRAVIS_BRANCH" != "master" ] || [ $TRAVIS_PHP_VERSION != "7.1" ]; then 
+if [ "$TRAVIS_PHP_VERSION" != "7.0" ]; then
+	echo "Skipping docker build on php7.0"
 	exit 0;
 fi
 
+TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+
 docker build -t weinstein/app -f deploy/app.docker .
 docker build -t weinstein/web -f deploy/web.docker .
+
+docker tag weinstein/app weinstein/app:$TAG
+docker tag weinstein/web weinstein/web:$TAG
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push weinstein/app
