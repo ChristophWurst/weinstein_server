fb065ce4d08e65f43becec062fd3d27d861e1203
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-22 23:16:03 +0100

    Directly add tag to prevent 'latest' pushes on branches
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- build/docker-images.sh
+++ build/docker-images.sh
@@ -10,11 +10,8 @@ fi
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo $BRANCH ; fi`
 
-docker build -t weinstein/app -f deploy/app.docker .
-docker build -t weinstein/web -f deploy/web.docker .
-
-docker tag weinstein/app weinstein/app:$TAG
-docker tag weinstein/web weinstein/web:$TAG
+docker build -t weinstein/app:$TAG -f deploy/app.docker .
+docker build -t weinstein/web:$TAG -f deploy/web.docker .
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push weinstein/app
