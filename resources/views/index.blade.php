8ef1ec83718acf7e83d5013c21ff03d9dab3d459
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-04-06 12:07:46 +0200

    Show version on start page
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,22 +2,23 @@
 
 @section('jumbotron')
 <div class="jumbotron">
-    <div class="container text-center">
-        <img height="120" width="120" src="{!! asset('img/ws-logo.svg') !!}">
-        <h1>Weinstein</h1>
-        @if (!Auth::check())
-        <p>
-            Willkommen!
-        </p>
-        <p>
-            Bitte melden Sie sich an
-        </p>
-        <p>
-            <a href="{!! route('login') !!}" class="btn btn-primary btn-lg" role="button">Anmelden</a>
-        </p>
-        @else
-        Entwickelt von Christoph Wurst, 2013 - 2017
-        @endif
-    </div>
+		<div class="container text-center">
+				<img height="120" width="120" src="{!! asset('img/ws-logo.svg') !!}">
+				<h1>Weinstein</h1>
+				@if (!Auth::check())
+				<p>
+						Willkommen!
+				</p>
+				<p>
+						Bitte melden Sie sich an
+				</p>
+				<p>
+						<a href="{!! route('login') !!}" class="btn btn-primary btn-lg" role="button">Anmelden</a>
+				</p>
+				@else
+				Version <?php echo config('app.version'); ?><br>
+				Entwickelt von Christoph Wurst, 2013 - 2017
+				@endif
+		</div>
 </div>
 @stop
