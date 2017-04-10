03c89db3a0fa0e6218d4d88fb2197ad7c58174ed
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-04-10 21:03:54 +0200

    Split Travis build to save some time
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ build/test_php.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ev
+
+# Check PHP syntax
+find . -name \*.php -not -path './vendor/*' -exec php -l "{}" \;
+
+# Generate secure key
+rm bootstrap/cache/compiled.php
+php artisan key:generate --no-interaction
+cat .env
+
+# Prepare DB - rollback once to catch potential migration/rollback errors
+php artisan migrate --no-interaction
+php artisan migrate:rollback --no-interaction
+php artisan migrate --no-interaction
+
+# Test PHP
+make test
+
+# Upload coverage results to scrutinizer
+wget https://scrutinizer-ci.com/ocular.phar
+php ocular.phar code-coverage:upload --format=php-clover tests/clover.xml
+
