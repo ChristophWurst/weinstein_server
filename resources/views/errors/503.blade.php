70d9410caeba1242c3be0dedb0aa0c2d98c28b1a
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-04-13 15:22:32 +0200

    Add better maintenance view/text
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Weinstein befindet sich im Wartungsmodus und wird in Kürze wieder verfügbar sein.</div>
             </div>
         </div>
     </body>
