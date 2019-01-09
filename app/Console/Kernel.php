70481929f3051a5d34af4a7a53328055ad0b01e5
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-01-09 21:54:42 +0100

    Add email test command
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\CreateUser;
+use App\Console\Commands\TestEmail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected $commands = [
 		CreateUser::class,
+		TestEmail::class,
 	];
 
 	/**
---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Success</h2>
+
+		<div>
+			As you're reading this, the email configuration seems to work.
+		</div>
+	</body>
+</html>
